61206f625c2a35dd500e8f9fc17ae12bc5db94e8
TomTilly <tomtillistrand@gmail.com>
2015-06-30 20:57:48 -0400

    Copy editting, minor script changes

--- README.md
+++ README.md
@@ -4,13 +4,13 @@ by Tom Tillistrand
 
 ### Overview
 
-This website contains a virtual "sketchpad" with an adjustable grid. By default, the grid is composed of a 16x16 set of small boxes, each of which activate when you hover your mouse over them. The effect that's activated depends on which "Hover Effect" you have selected.
+This website contains a virtual sketchpad. By default, the sketchpad is composed of a 16x16 set of small boxes, each of which activate when you hover your mouse over them. The effect that's activated depends on which "Hover Effect" you have selected.
 
-### Resetting the Grid
+### Resetting the sketchpad
 
-Pressing "Reset Grid" allows you to resize the dimensions of the grid. The higher the dimensions, the smaller the individual box size and greater fine grain control you have.
+Pressing "Reset sketchpad" allows you to resize the resolution of the sketchpad. The higher the resolution, the smaller the individual box size and greater fine grain control you have.
 
-After pressing "Reset Grid", you'll be prompted to input the size of the grid you want to work with. Since the grid is a square, you only need to enter one of the dimensions. For example, if you wanted to work with a grid of 40x40 boxes, you would enter "40" into the input.
+After pressing "Reset", you'll be prompted to input the resolution you'd like to work with. Since the sketchpad is a square, you only need to enter one of the dimensions. For example, for a sketchpad that's 40 x 40, you would enter "40" into the input.
 
 ### Hover Effects
 
@@ -21,8 +21,12 @@ There are four different hover effects.
 3. Trail - Each box fades in to black and fades out to white.
 4. Shading - Each box increases in opacity by 10%.
 
-The selected effect will activate once "Reset Grid" is pressed.
+The selected effect will activate once "Reset" is pressed.
 
 ### Build Notes
 
-This site was created as an exercise in JavaScript and jQuery DOM manipulation. Each individual box within the grid is generated in JavaScript using jQuery. The ability to resize the dimensions of the grid within the same confines, and to change the effects on mouse over, also required JavaScript and jQuery.
+This site was created as an exercise in JavaScript and jQuery DOM manipulation. Each individual box within the sketchpad is generated in JavaScript using jQuery. The ability to resize the resolution of the sketchpad within the same confines, and to change the effects on mouse over, also required JavaScript and jQuery.
+
+### Credits
+
+All code created by Tom Tillistrand. Contact at ny.prosource@gmail.com
--- javascript/main-script.js
+++ javascript/main-script.js
@@ -1,73 +1,121 @@
+/**
+
+Author: Tom Tillistrand
+Date: June, 2015
+Description: This script contains the major functionality for a virtual sketchpad. Users can draw by hovering their mouse over a sketchpad of boxes (div elements), causing them to change color. The user can change the dimensions of the sketchpad and the effect used when drawing on the sketchpad.
+
+*/
+
 $(document).ready(function(){
+	
+	// Initialize sketchpad by adding 16x16 sketchpad of boxes
+	
 	for(var i = 0; i < 256; i++){
-		$('.grid').append('<div class="grid-box"></div>');
+		$('.sketchpad').append('<div class="pixels"></div>');
 	}
+	
+	// Check which hover effect option the user has selected
 	
 	function getOption(){
 		return $('input:checked').val();
 	}
 	
+	/* 
+	
+	Description: Depending on the effect chosen by the user, this function sets the corresponding CSS styles and event handlers on the sketchpad divs to enable the hover effect.
+	Parameter: option - The hover effect option selected by the user
+	
+	*/
+	
 	function addHoverEvent(option){
-		$('.grid-box').on('mouseover', function(){
-			switch (option){
-				case 'standard':
+		switch (option){
+			
+			// If user has selected the "Standard" option
+			
+			case 'standard':
+				$('.pixels').css({
+					'background-color': 'white',
+					'opacity': '1'
+				});
+				$('.pixels').on('mouseover', function(){
 					$(this).css("background-color", "#3947e6");
-					break;
-				case 'rando':
+				});
+				break;
+				
+			// If user has selected the "Random Colors" option
+				
+			case 'rando':
+				$('.pixels').css({
+					'background-color': 'white',
+					'opacity': '1'
+				});
+				$('.pixels').on('mouseover', function(){
 					var color = "#" + (Math.floor(Math.random() * 10)) + (Math.floor(Math.random() * 10)) + (Math.floor(Math.random() * 10));
 					$(this).css("background-color", color);
-					break;
-				case 'shade':
+				});
+				break;
+				
+			// If user has selected the "Shading" option
+				
+			case 'shade':
+				$('.pixels').css({
+					'background-color': 'black',
+					'opacity': '0'
+				});
+				$('.pixels').on('mouseover', function(){
 					$(this).css("opacity", '+=.1');
-					break;
-				case 'trail':
+				});
+				break;
+				
+			// If user has selected the "Fade" option
+				
+			case 'fade':
+				$('.pixels').css({
+					'background-color': '#333',
+					'opacity': '0'
+				});
+				$('.pixels').on('mouseover', function(){
 					$(this).animate({
-						opacity: 1
-					},200);
-					/*$(this).on('mouseout', function(){
-						$(this).animate({
-						opacity: 0
+					opacity: 1
 					}, 200);
-			});*/
-					break;
-			}
-		});
+				});
+				$('.pixels').on('mouseout', function(){
+					$(this).animate({
+					opacity: 0
+					}, 200);
+				});
+				break;
+		}
 	}
 	
-	$('button').on('click', function(e){
-		e.preventDefault();
-		$('.grid-box').remove();
-		var size = prompt("What size grid do want?");
-		var gridSize = size * size;
-		var boxSize = 480/size;
-		for(var i = 0; i < gridSize; i++){
-		$('.grid').append('<div class="grid-box"></div>');
-		}
-		var option = getOption();
-		if (option === 'shade') {
-			$('.grid-box').css({
-				'background-color': 'black',
-				'opacity': '0'
-			});
-		} else if (option === 'trail'){
-			$('.grid-box').css({
-				'background-color': '#333',
-				'opacity': '0'
-			});
-			$('grid-box').on('mouseout', function(){
-				$(this).animate({
-					opacity: 0
-				}, 500);
-			});
-		} else {
-			$('.grid-box').css({
-				'background-color': 'white',
-				'opacity': '1'
-			});
-		}
-		$('.grid-box').width(boxSize + "px").height(boxSize + "px");
-		addHoverEvent(option);
-	});
+	// Set event handler on "Reset" button for when it's clicked.
 	
-	addHoverEvent(getOption());
+	$('button').on('click', function(e){
+		$('.sketchpad').html('').hide();						// Clear the sketchpad of all pixels (divs) and hide sketchpad temporarily
+		// Prompt user for new dimensions
+		var size = prompt("Enter a resolution for the sketchpad (for a sketchpad with the dimensions 30 x 30, enter the number '30')");
+		for(var i = 0; i < size.length; i++){					// Validate user input to check to see if text was entered
+			if (size[i] !== '0' && size[i] !== '1' && size[i] !== '2' && size[i] !== '3' && size[i] !== '4' && size[i] !== '5' && size[i] !== '6' && size[i] !== '7' && size[i] !== '8' && size[i] !== '9'){
+				size = 'Not a number';
+				break;
+			}
+		}
+		
+		if (size < 1 || size > 100){								// Did the user enter a number below 1 or above 100?
+			$('.sketchpad').html('<p class="center">Please enter a number from 1 to 100</p>').show();
+		} else if(size === 'Not a number'){							// Did the user enter text?
+			$('.sketchpad').html('<p class="center">Please enter a valid number</p>').show();
+		} else {													// User entry is acceptable, create new sketchpad with new resolution and drawing style
+			var sketchpadSize = size * size;						// Number of boxes in new sketchpad
+			var boxSize = 480/size;									// Actual pixel size of each box in new sketchpad
+			for(var i = 0; i < sketchpadSize; i++){					// Add boxes to sketchpad
+				$('.sketchpad').append('<div class="pixels"></div>');
+			}
+			$('.pixels').width(boxSize + "px").height(boxSize + "px");	// Set new pizel size for each box
+			addHoverEvent(getOption());								// Call addHoverEvent to set a hover effect on each box
+			$('.sketchpad').show();									// Redisplay sketchpad now that it has been recreated
+		}
+	});
+
+	addHoverEvent(getOption());										// Set initial hover effect on each box
 });
